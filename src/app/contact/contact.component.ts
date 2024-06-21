09498169d1a14de50d1557d62dcb40b4eef818eb
dungDTT <hqhplxag@gmail.com>
2024-06-21 07:29:10 +0700

    add json-server

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-//import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';  
+import axios from 'axios';
 
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -14,42 +15,52 @@ import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } f
 
 export class ContactComponent {
   
-  userForm!: FormGroup;
-
-  constructor(private fb: FormBuilder){
-    this.userForm = this.fb.group({
-      fullname:'',
-      email:'',
-      contact:'',
-      subjects: this.fb.array([])
-    })
-
-  }
-
+  userForm: FormGroup;
+  currentId = 1;
   result = '';
+  users: any[] = [];
 
-  getSubjectGroup(){
-    return this.fb.group({
-      name: '',
-      status:'',
-      marks:''
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      //id: [0],
+      id_user: ['', Validators.required],
+      avatar: [''],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      joinDate: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.getAllUser(); // Lấy danh sách người dùng khi component khởi tạo
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const formData = this.userForm.value;
+      axios.post('https://jsonplaceholder.typicode.com/users', formData)
+        .then(response => {
+          this.result = 'Thêm thành công!';
+        })
+        .catch(error => {
+          this.result = 'Đã xảy ra lỗi';
+          console.error(error);
+        });
+    } else {
+      this.result = 'Form không hợp lệ!';
+    }
+  }
+
+  getAllUser(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      this.result = 'Get all thành công!';
     })
-  }
-
-  get subjects(){
-    return this.userForm.get('subjects') as FormArray;
-  }
-
-  AddSubjects(){
-    this.subjects.push(this.getSubjectGroup());
-  }
-
-  RemoveSubjects(indexAt: number){
-    this.subjects.removeAt(indexAt);
-  }
-
-  onSubmit(){
-    this.result = JSON.stringify(this.userForm.value);
+    .catch(error => {
+      this.result = 'Đã xảy ra lỗi';
+      console.error(error);
+    });
   }
 
 }
